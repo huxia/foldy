94fa3b5e443dac8a17d0f52e14ad2a91a75c6950
huizhe <huizhe.xiao@gmail.com>
2016-09-18 23:27:23 +0800

    Constants class should not be referencing any other class

--- src/Constants.php
+++ src/Constants.php
@@ -1,16 +1,14 @@
 <?php
 namespace Foldy;
 
-use Foldy\Data\DB;
-
 class Constants
 {
     const DI_KEY_FUNC_IS_DIR = 'is_dir';
     const DI_KEY_FUNC_SCANDIR = 'scandir';
     const DI_KEY_FUNC_INCLUDE = 'include';
-    const DI_KEY_CLASS_REQUEST = Request::class;
-    const DI_KEY_CLASS_RESPONSE = Response::class;
-    const DI_KEY_CLASS_DB = DB::class;
+    const DI_KEY_CLASS_REQUEST = 'class:request';
+    const DI_KEY_CLASS_RESPONSE = 'class:response';
+    const DI_KEY_CLASS_DB = 'class:db';
 
     const DI_KEY_EXCEPTION_CHECK_INPUT_ERROR = 'error:check_input';
 
